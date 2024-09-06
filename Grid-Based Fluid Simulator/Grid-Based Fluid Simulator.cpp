e636303f5f214c563944ca389c2b51698835d01e
Szymon Żuber <tornaos21@gmail.com>
2024-09-06 20:02:31 +0200

    working grid showing colors

--- Grid-Based Fluid Simulator/Grid-Based Fluid Simulator.cpp
+++ Grid-Based Fluid Simulator/Grid-Based Fluid Simulator.cpp
@@ -1,21 +1,46 @@
 #include "raylib.h"
 
+constexpr int SCREENWIDTH = 1600;
+constexpr int SCREENHEIGHT = 900;
+constexpr int FRAMERATE = 60;
+constexpr int gridCellSize = 5;
+
+Color colorArray[9];
+
+typedef struct gridCell {
+    Color cellColor;
+}gridCell;
+
+gridCell Grid[SCREENHEIGHT / gridCellSize][SCREENWIDTH / gridCellSize];
+
+void initScreen() {
+    InitWindow(SCREENWIDTH, SCREENHEIGHT, "Fluid Simulator");
+    SetTargetFPS(FRAMERATE);
+}
+
+void drawScreen() {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for (int col = 0; col < SCREENHEIGHT / gridCellSize; col++) {
+        for (int row = 0; row < SCREENWIDTH / gridCellSize; row++) {
+            DrawRectangle(gridCellSize * row, gridCellSize * col, gridCellSize, gridCellSize, Grid[col][row].cellColor);
+        }
+    }
+    EndDrawing();
+}
+
 int main(void)
 {
-
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);   
-
-    while (!WindowShouldClose())   
+    colorArray[0] = BLACK;
+    for (int col = 0; col < SCREENHEIGHT / gridCellSize; col++) {
+        for (int row = 0; row < SCREENWIDTH / gridCellSize; row++) {
+            Grid[col][row].cellColor = BLACK;
+        }
+    }
+    initScreen();
+    while (!WindowShouldClose())
     {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
+        drawScreen();
     }
     CloseWindow();        
     return 0;
